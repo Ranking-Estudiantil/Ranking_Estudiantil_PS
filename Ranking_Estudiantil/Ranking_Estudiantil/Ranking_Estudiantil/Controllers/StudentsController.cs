3dfa0594f4d225da18cf8a783977508950976ecd
DevinAmurrioLizarazu <111919106+DevinAmurrioLizarazu@users.noreply.github.com>
2022-10-26 23:10:03 -0400

    Añadi front end de las tablas y de los formularios de edicion

---
+++ Ranking_Estudiantil/Ranking_Estudiantil/Ranking_Estudiantil/Controllers/StudentsController.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Ranking_Estudiantil.Data;
+using Ranking_Estudiantil.Models;
+
+namespace Ranking_Estudiantil.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public StudentsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Students
+        public async Task<IActionResult> Bronze()
+        {
+            IQueryable<Student> student = from Student in _context.Students
+                                          where Student.Rank == 1
+                                          select Student;
+                                        
+
+            return View(await student.ToListAsync());
+        }
+        public async Task<IActionResult> Silver()
+        {
+            IQueryable<Student> student = from Student in _context.Students
+                                          where Student.Rank == 2
+                                          select Student;
+
+
+            return View(await student.ToListAsync());
+        }
+        public async Task<IActionResult> Gold()
+        {
+            IQueryable<Student> student = from Student in _context.Students
+                                          where Student.Rank == 3
+                                          select Student;
+
+
+            return View(await student.ToListAsync());
+        }
+        public async Task<IActionResult> Platinum()
+        {
+            IQueryable<Student> student = from Student in _context.Students
+                                          where Student.Rank == 4
+                                          select Student;
+
+
+            return View(await student.ToListAsync());
+        }
+        public async Task<IActionResult> Diamond()
+        {
+            IQueryable<Student> student = from Student in _context.Students
+                                          where Student.Rank == 5
+                                          select Student;
+
+
+            return View(await student.ToListAsync());
+        }
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Students.Include(s => s.career);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Students/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Students == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Students
+                .Include(s => s.career)
+                .FirstOrDefaultAsync(m => m.StudentID == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return View(student);
+        }
+
+        // GET: Students/Create
+        public IActionResult Create()
+        {
+            ViewData["CareerID"] = new SelectList(_context.Careers, "CareerID", "CareerName");
+            return View();
+        }
+
+        // POST: Students/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Rank,Score,CareerID")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(student);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CareerID"] = new SelectList(_context.Careers, "CareerID", "CareerName", student.CareerID);
+            return View(student);
+        }
+
+        // GET: Students/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Students == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Students.FindAsync(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            ViewData["CareerID"] = new SelectList(_context.Careers, "CareerID", "CareerName", student.CareerID);
+            return View(student);
+        }
+
+        // POST: Students/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("StudentID,Rank,Score,CareerID")] Student student)
+        {
+            if (id != student.StudentID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(student);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!StudentExists(student.StudentID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CareerID"] = new SelectList(_context.Careers, "CareerID", "CareerName", student.CareerID);
+            return View(student);
+        }
+
+        // GET: Students/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Students == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Students
+                .Include(s => s.career)
+                .FirstOrDefaultAsync(m => m.StudentID == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return View(student);
+        }
+
+        // POST: Students/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Students == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Students'  is null.");
+            }
+            var student = await _context.Students.FindAsync(id);
+            if (student != null)
+            {
+                _context.Students.Remove(student);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool StudentExists(int id)
+        {
+          return _context.Students.Any(e => e.StudentID == id);
+        }
+    }
+}
---
+++ Ranking_Estudiantil/Ranking_Estudiantil/Ranking_Estudiantil/Data/ApplicationDbContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Ranking_Estudiantil.Models;
+
+namespace Ranking_Estudiantil.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+
+        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public  DbSet<AcademicUnity> AcademicUnities { get; set; }
+       
+        public  DbSet<Career> Careers { get; set; }
+        public  DbSet<Department> Departments { get; set; } 
+        public  DbSet<Faculty> Faculties { get; set; } 
+        public  DbSet<Student> Students { get; set; }
+        public DbSet<Person> People { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AcademicUnity>().ToTable("AcademicUnity");
+            modelBuilder.Entity<Career>().ToTable("Career");
+            modelBuilder.Entity<Department>().ToTable("Department");
+            modelBuilder.Entity<Faculty>().ToTable("Faculty");
+            modelBuilder.Entity<Student>().ToTable("Student");
+            modelBuilder.Entity<Person>().ToTable("Person");
+
+        }
+        public Person ValidarUsuario(string _correo, string _password)
+        {
+
+            return People.Where(item => item.Email == _correo && item.Password == _password).FirstOrDefault();
+        }
+    }
+}
