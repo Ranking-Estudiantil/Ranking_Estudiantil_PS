5f748d1d61bf040432f9a225b951da24ed383301
DevinAmurrio <111919106+DevinAmurrioLizarazu@users.noreply.github.com>
2022-11-02 13:54:52 -0400

    Segunda version del frontend

---
+++ Ranking_Estudiantil_Segunda_Version_Frontend/Ranking_Estudiantil/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace Ranking_Estudiantil.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public byte Rank { get; set; }
+        public short Score { get; set; }
+        public int CareerID { get; set; }
+        public Career? career { get; set; }
+    }
+}
