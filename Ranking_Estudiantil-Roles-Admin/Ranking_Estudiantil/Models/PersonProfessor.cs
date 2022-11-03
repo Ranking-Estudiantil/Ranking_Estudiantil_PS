283b6072cb952be0f9bc3f1337b3b677edd38cc3
RafaelJaillita <111938727+RafaelJaillita@users.noreply.github.com>
2022-11-03 08:59:54 -0400

    Menejo de roles,creacion de adminstradores

---
+++ Ranking_Estudiantil-Roles-Admin/Ranking_Estudiantil/Models/PersonProfessor.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ranking_Estudiantil.Models
+{
+    public class PersonProfessor
+    {
+        [Key]
+        public int? PersonID { get; set; }
+        [Required(ErrorMessage = "First Name is required")]
+        [StringLength(50, ErrorMessage = "{0} must be: minimum {2} and maximum {1}", MinimumLength = 3)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Last Name is required")]
+        [StringLength(50, ErrorMessage = "{0} must be: minimum {2} and maximum {1}", MinimumLength = 3)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "Second Last Name is required")]
+        [StringLength(50, ErrorMessage = "{0} must be: minimum {2} and maximum {1}", MinimumLength = 3)]
+        [Display(Name = "Second Last Name")]
+        public string SecondLastName { get; set; }
+        public byte Status { get; set; }
+
+        public DateTime RegisterDate { get; set; }
+        public int Role { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int ProfessorID { get; set; }
+        public int CareerID { get; set; }
+        public Career? career { get; set; }
+    }
+}
