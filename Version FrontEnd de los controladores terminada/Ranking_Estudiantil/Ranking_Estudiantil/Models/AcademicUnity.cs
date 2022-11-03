cca9c72d2dbf0c16eb2a172f34ab37642e8c5e42
AnthonyEZ <anthonito223@gmail.com>
2022-11-03 08:58:52 -0400

    FrontEnd de los CRUDS
    Se realizo el FrontEnd del editado de estudiantes

---
+++ Version FrontEnd de los controladores terminada/Ranking_Estudiantil/Ranking_Estudiantil/Models/AcademicUnity.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ranking_Estudiantil.Models
+{
+    public class AcademicUnity
+    {
+        [Key]
+        public int AcademicUnityID { get; set; }
+        [Required(ErrorMessage = "Debe llenar este campo")]
+        [StringLength(50, ErrorMessage = "{0} debe ser minimo de {2} caracteres y maximo {1}", MinimumLength = 3)]
+        [Display(Name = "Unidad Academica")]
+        public string AcademicUnityName { get; set; } = null!;
+        public ICollection<Faculty>? Faculties { get; set; }
+
+    }
+}
