d708fca4bc846acfa5ca572223663e1123a7f16c
DevinAmurrio <111919106+DevinAmurrioLizarazu@users.noreply.github.com>
2022-11-03 00:21:00 -0400

    Añadido la función de enviar email

---
+++ Ranking_Estudiantil_Envio_Email/Ranking_Estudiantil/Ranking_Estudiantil/Ranking_Estudiantil/Controllers/PeopleController.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Net.Security;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Ranking_Estudiantil.Data;
+using Ranking_Estudiantil.Models;
+using System;
+using System.Threading;
+
+namespace Ranking_Estudiantil.Controllers
+{
+    public class PeopleController : Controller
+    {
+        string pass;
+        string email;
+        private readonly ApplicationDbContext _context;
+
+        public PeopleController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: People
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.People.ToListAsync());
+        }
+
+        // GET: People/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.People == null)
+            {
+                return NotFound();
+            }
+
+            var person = await _context.People
+                .FirstOrDefaultAsync(m => m.PersonID == id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return View(person);
+        }
+
+        // GET: People/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public void sendMail()
+        {
+            //Send Email-----------------------------------------------------------------------------
+            MailMessage correo = new MailMessage();
+            correo.From = new MailAddress("devinamurrioUnivalle@gmail.com", "Kyocode", System.Text.Encoding.UTF8);//Correo de salida
+            correo.To.Add(email); //Correo destino?
+            correo.Subject = "Datos de Login"; //Asunto
+            correo.Body = "Password: " + pass; //Mensaje del correo
+            correo.IsBodyHtml = true;
+            correo.Priority = MailPriority.Normal;
+            SmtpClient smtp = new SmtpClient();
+            smtp.UseDefaultCredentials = false;
+            smtp.Host = "smtp.gmail.com"; //Host del servidor de correo
+            smtp.Port = 587; //Puerto de salida
+            smtp.Credentials = new NetworkCredential("devinamurrioUnivalle@gmail.com", "zpksehcoeyqqqsol");//Cuenta de correo
+            ServicePointManager.ServerCertificateValidationCallback = delegate (object s, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors) { return true; };
+            smtp.EnableSsl = true;//True si el servidor de correo permite ssl
+            smtp.Send(correo);
+        }
+
+        // POST: People/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("PersonID,FirstName,LastName,SecondLastName,Status,RegisterDate,Role,Email,Password")] Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(person);
+                pass = person.Password;
+                email = person.Email;
+
+                Thread backgroundThread = new Thread(new ThreadStart(sendMail));
+                backgroundThread.Start();
+
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(person);
+        }
+
+        // GET: People/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.People == null)
+            {
+                return NotFound();
+            }
+
+            var person = await _context.People.FindAsync(id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+            return View(person);
+        }
+
+        // POST: People/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("PersonID,FirstName,LastName,SecondLastName,Status,RegisterDate,Role,Email,Password")] Person person)
+        {
+            if (id != person.PersonID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(person);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PersonExists(person.PersonID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(person);
+        }
+
+        // GET: People/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.People == null)
+            {
+                return NotFound();
+            }
+
+            var person = await _context.People
+                .FirstOrDefaultAsync(m => m.PersonID == id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return View(person);
+        }
+
+        // POST: People/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.People == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.People'  is null.");
+            }
+            var person = await _context.People.FindAsync(id);
+            if (person != null)
+            {
+                _context.People.Remove(person);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PersonExists(int id)
+        {
+          return _context.People.Any(e => e.PersonID == id);
+        }
+    }
+}
---
+++ Ranking_Estudiantil_Envio_Email/Ranking_Estudiantil/Ranking_Estudiantil/Ranking_Estudiantil/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ranking_Estudiantil.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
