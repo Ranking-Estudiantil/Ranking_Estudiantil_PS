e99307f7467002dd59b84134c1dec79db83c4df4
RafaelJaillita <111938727+RafaelJaillita@users.noreply.github.com>
2022-11-03 08:58:22 -0400

    Incremento de puntaje de los estudiantes y vista de del ranking

--- Ranking_Estudiantil-Roles-Admin/Ranking_Estudiantil/Models/Bronze.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Ranking_Estudiantil.Models
-{
-    public class Bronze
-    {
-        public short? score { get; set; }
-        public byte? Rank { get; set; }
-    }
-}
--- Ranking_Estudiantil-Roles-Admin/Ranking_Estudiantil/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Ranking_Estudiantil.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
