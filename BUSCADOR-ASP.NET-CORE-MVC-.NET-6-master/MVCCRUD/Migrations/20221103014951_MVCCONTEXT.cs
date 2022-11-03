ed86f80013a6ab70dd0c4e5a8272887a51ea3a29
AnthonyEZ <anthonito223@gmail.com>
2022-11-03 08:56:22 -0400

    Implementacion del Buscador
    Se realizo un search para facilitar la busqueda de estudiantes en el  Rankig

---
+++ BUSCADOR-ASP.NET-CORE-MVC-.NET-6-master/MVCCRUD/Migrations/20221103014951_MVCCONTEXT.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MVCCRUD.Migrations
+{
+    public partial class MVCCONTEXT : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    Apellido = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    Fecha = table.Column<DateTime>(type: "date", nullable: true),
+                    Clave = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
